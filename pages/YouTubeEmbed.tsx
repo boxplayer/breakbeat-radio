6da4ad5f11af691b28930c54e5cd8da232240143
jwadolow94 <jan.wadolowski@alpima.net>
2022-07-23 20:53:02 +0200

    added youtube embed component

---
+++ pages/YouTubeEmbed.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const YoutubeEmbed = ({ embedId }: { embedId: string }) => (
+  <div className="video-responsive">
+    <iframe
+      width="853"
+      height="480"
+      src={`https://www.youtube.com/embed/${embedId}?autoplay=1`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,33 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { YoutubeEmbed } from "./YouTubeEmbed";
+
+const coffeeShopLofiId = "-5KAN9_CzSA";
 
 const Home: NextPage = () => {
+  const [play, setPlay] = useState(true);
+
+  console.log({ play });
+
+  useEffect(() => {
+    function handleKeyDown(e: any) {
+      console.log(e.keyCode);
+      if (e.keyCode === 32) {
+        setPlay(!play);
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    // Don't forget to clean up
+    return function cleanup() {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [play]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,13 +36,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <YoutubeEmbed embedId={coffeeShopLofiId} />
+
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
@@ -59,14 +85,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
