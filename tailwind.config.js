47070df03ec40f699247ef725b8995b36adba847
boxplayer <j_wadolowski@hotmail.com>
2024-06-15 19:14:46 +0200

    converted to es6 mod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 //TODO: remove backgroundImage?
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    dropShadow: {
-      radio: "2px 2px var(--tw-shadow-color)",
-    },
-    extend: {
-      backgroundImage: {
-        "main-wall": "url('/lofi-wallpaper.jpg')",
-      },
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  dropShadow: {
+    radio: "2px 2px var(--tw-shadow-color)",
+  },
+  extend: {
+    backgroundImage: {
+      "main-wall": "url('/lofi-wallpaper.jpg')",
     },
   },
-  plugins: [],
 };
+export const plugins = [];
